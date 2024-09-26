48ee3fb5c18fe6da01b66e9c5b54860dddbcdd41
Piotr Pieróg <ppierog@gmail.com>
2024-09-26 06:41:20 +0200

    Itoa + strings.asm
    Write itoa in nasm and C, initialize strings.asm

---
+++ nasm/strings/test.asm
@@ -0,0 +1,68 @@
+bits 64
+
+extern printf
+extern __itoa
+extern __strlen
+extern __strcat
+
+section .rodata
+  format1       db      "%s",0xA, 0x0
+  format2       db      "%d",0xA, 0x0
+
+
+section .data
+
+  align 8
+  sbuff       db      0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0
+
+  tstring     db      "1234567890abcdef",0x0
+
+
+section .bss
+
+section .text
+
+    global  _start
+
+_start:
+
+  ; https://en.wikibooks.org/wiki/X86_Assembly/Interfacing_with_Linux
+  ; arguments rdi, rsi, rdx, rcx, r8, r9
+  ; https://wiki.osdev.org/System_V_ABI
+
+    mov rdi, 123456789
+    mov rsi, sbuff
+    mov rdx, 10
+
+    call __itoa
+
+    mov rdi, format1
+    mov rsi, sbuff
+    call printf
+
+    mov rdi, tstring
+    call __strlen
+    mov rsi , rax
+
+    mov rdi , format2
+    call printf
+
+    mov byte [sbuff], 0x0
+    mov rdi, sbuff
+    mov rsi, tstring
+    call __strcat
+
+    mov rdi, sbuff
+    mov rsi, tstring
+    call __strcat
+
+    mov rdi, format1
+    mov rsi, sbuff
+    call printf
+
+
+
+
+    mov     rbx, 0            ; exit -> first syscall argument: exit code
+    mov     rax, 1            ; system call number (sys_exit)
+    int     0x80              ; call kernel
